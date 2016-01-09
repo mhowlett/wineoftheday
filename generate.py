9b8fd40a3ae8ae0d0a228a43e9ef1dde2b4a4605
Matt Howlett <matt@mhowlett.com>
2016-01-10 08:09:35 +1100

    a few tweaks

--- generate.py
+++ generate.py
@@ -3,28 +3,29 @@ import nltk
 with open ("qualities.txt", "r") as myfile:
     data=myfile.readlines()
 
-len = 50000
+nToks = 50000
 content_text = ' '.join(data)
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+|[^\w\s]+')
 tokenized_content = tokenizer.tokenize(content_text)
 content_model = nltk.NgramModel(3, tokenized_content)
-content = content_model.generate(len)
+content = content_model.generate(nToks)
 
 cSentence = ""
 numberSentencesToIgnore = 3
 
 with open("output.txt", "w") as text_file:
-    for x in range(0, len):
+    for x in range(0, nToks):
         next = ''
-        if (x < len-1):
+        if (x < nToks-1):
             next = content[x+1]
         current = content[x]
 
         cSentence = cSentence + current
-        if current == ".":
+        if current == "." or current == "%":
             numberSentencesToIgnore = numberSentencesToIgnore - 1
             if numberSentencesToIgnore < 0:
-                text_file.write(cSentence + "\n")
+                if len(cSentence) > 4:
+                    text_file.write(cSentence + "\n")
             cSentence = ""
             continue
 
